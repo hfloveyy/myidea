b4a4ec06d201bf7e4cddf346424e68c72a10a8ab
hf <hfloveyy@icloud.com>
2017-04-06 15:46:13 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import itchatmp
+
+itchatmp.update_config(itchatmp.WechatConfig(
+    token='iloveyy1314',
+    appId = 'wx9e77f84e28b0a204',
+    appSecret = '68c024da4eed3a948a3f2c0a0b6d5f72'))
+
+@itchatmp.msg_register(itchatmp.content.TEXT)
+def text_reply(msg):
+    return msg['Content']
+
+
+if __name__ == '__main__':
+    itchatmp.run()
