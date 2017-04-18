376a354e3906c4b35211a4708b222343c3dd51c4
hf <hfloveyy@icloud.com>
2017-04-18 16:01:35 +0800

    requirements.txt

--- getVideo.py
+++ getVideo.py
@@ -1,9 +1,38 @@
 #-*- coding:utf-8 -*-
 from pytube import YouTube
 
-yt = YouTube("http://www.youtube.com/watch?v=Ik-RsDGPI5Y")
+# pprint-pretty print 不必要，仅仅为了让输出更好看，每个视频文件占一行
+from pprint import pprint
 
-# Once set, you can see all the codec and quality options YouTube has made
-# available for the perticular video by printing videos.
+yt = YouTube("https://www.youtube.com/watch?v=Ls5pgieFNHs")
 
-print(yt.get_videos())
+# 显示所有可以下载的视频文件
+pprint(yt.get_videos())
+
+# 显示视频文件名
+print(yt.filename)
+'''
+# 设置视频文件名
+yt.set_filename('myFirstVideo')
+
+# 根据文件类型过滤视频文件
+pprint(yt.filter('flv'))
+
+# 由于排序是按清晰度从低到高，所以可以用 -1 索引到最高清版本
+print(yt.filter('.mp4')[-1])
+
+# 根据清晰度过滤文件
+pprint(yt.filter(resolution='480p'))
+
+# 通过文件类型和清晰度指定下载的视频
+video = yt.get('mp4','720p')
+
+# 如果有多个相同类型，或者相同清晰度的文件，则不能仅指定一种格式来下载视频，例如下面一行可能会报错：
+video = yt.get('mp4')
+'''
+# 其实，上面的 video 完全可以用过滤+索引的方式获得，不一定非得用 get 方法
+pprint(yt.filter(resolution='360p'))
+video = yt.get('mp4','360p')
+pprint(video)
+# 下载到指定路径
+video.download('/home/hf')
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ itchatmp.update_config(itchatmp.WechatConfig(
     token='iloveyy1314',
     appId = 'wx9e77f84e28b0a204',
     appSecret = '68c024da4eed3a948a3f2c0a0b6d5f72'))
-
+'''
 @itchatmp.msg_register(itchatmp.content.TEXT)
 def text_reply(msg):
     return msg
@@ -13,3 +13,14 @@ def text_reply(msg):
 
 if __name__ == '__main__':
     itchatmp.run()
+'''
+
+infoDict = {
+    'Title' : u'exploit',
+    'Description' : u'exploit windows7 through winrar', }
+r = itchatmp.messages.upload(itchatmp.content.VIDEO, 'videos/myvideo.mp4',
+    additionalDict=infoDict, permanent=True)
+if r:
+    print(r['media_id'])
+else:
+    print('Failed: \n%s' % r)
